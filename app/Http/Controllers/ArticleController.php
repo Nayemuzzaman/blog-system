942148b59952b7f2fe2e2d6ded8eff1f57d88e69
nayemuzzaman <nayemzaman@gmail.com>
2026-05-27 18:13:39 +0900

    pr3

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function createConfirm(Request $request)
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $article)
+    {
+        //
+    }
+
+    public function edit(string $article)
+    {
+        //
+    }
+
+    public function editConfirm(Request $request, string $article)
+    {
+        //
+    }
+
+    public function update(Request $request, string $article)
+    {
+        //
+    }
+
+    public function deleteConfirm(string $article)
+    {
+        //
+    }
+
+    public function destroy(string $article)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ArticleController::class, 'index'])->name('articles.home');
+
+Route::prefix('articles')->name('articles.')->controller(ArticleController::class)->group(function () {
+    Route::post('create_confirm', 'createConfirm')->name('create_confirm');
+    Route::put('{article}/edit_confirm', 'editConfirm')->name('edit_confirm');
+    Route::get('{article}/delete_confirm', 'deleteConfirm')->name('delete_confirm');
 });
+
+Route::resource('articles', ArticleController::class);
